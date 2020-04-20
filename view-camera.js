d62aadb7a535a0cdc5277a4acde13871bd3180ab
letrungkien <ltk.business@gmail.com>
2020-04-20 10:44:55 +0900

    Update view-camera.js
    follow fgnoe

--- view-camera.js
+++ view-camera.js
@@ -1,5 +1,5 @@
 let cameras = [];
-let camId = 1;
+let camId = 0;
 let currentStream = null;
 
 document.addEventListener('readystatechange', (event) => {
@@ -7,6 +7,7 @@ document.addEventListener('readystatechange', (event) => {
 	if(document.readyState === 'complete') {
 
 		const video = document.querySelector('video');
+		const body = document.querySelector('body');
 
 		function successCallback(stream) {
 		  currentStream = stream;
@@ -25,50 +26,57 @@ document.addEventListener('readystatechange', (event) => {
 		  .then(successCallback)
 		  .catch(errorCallback);
 
-		if('mediaDevices' in navigator && 'enumerateDevices' in navigator.mediaDevices) {
-			if(navigator.mediaDevices.enumerateDevices) {
-				navigator.mediaDevices.enumerateDevices().then(media_devices => {
-					media_devices.forEach(media_device => {
-						if(location.href.includes('&debug')) {
-							console.log(media_device);
-						}
-				    if(media_device.kind === 'videoinput') {
-					   	cameras = cameras.concat(media_device.deviceId);
-						}
+		let reloadCameras = () => {
+			console.log('reloading cameras');
+			cameras = [];
+			if('mediaDevices' in navigator && 'enumerateDevices' in navigator.mediaDevices) {
+				if (navigator.mediaDevices.enumerateDevices) {
+					navigator.mediaDevices.enumerateDevices().then(media_devices => {
+						media_devices.forEach(media_device => {
+							if (location.href.includes('&debug')) {
+								console.log(media_device);
+							}
+							if (media_device.kind === 'videoinput') {
+								cameras = cameras.concat(media_device.deviceId);
+							}
+						})
 					})
-		    })
-		  }
-		}
-
-		video.addEventListener('dblclick',event => {
-			if(location.href.includes('&debug')) {
-				console.log('clicked on video');
-				console.log(cameras);
-				console.log(camId);
-				console.log(currentStream);
+				}
 			}
+			console.log(cameras);
+		};
+
+		let changeVideo = event => {
+			console.log('changing camera');
 			if((camId + 1) < cameras.length) {
 				camId = camId +1;
 			} else {
 				camId = 0;
 			}
+			if (cameras.length < 2) {
+				reloadCameras();
+			}
 			if(cameras.length > 1) {
 				if(navigator.mediaDevices || navigator.mediaDevices.enumerateDevices) {
 					currentStream.getTracks().forEach(track => {
 						track.stop();
-  				});
+					});
 					video.srcObject = null;
 					navigator.mediaDevices.getUserMedia({
 						audio: false,
 						video: {
-								deviceId: {
-									exact: cameras[camId]
-								}
-							}
+							deviceId: {
+								exact: cameras[camId]
+							},
+							width: { ideal: 4096 },
+							height: { ideal: 2160 }
+						}
 					}).then(successCallback).catch(errorCallback);
 				}
 			}
-		});
+		};
+
+		body.addEventListener('dblclick', changeVideo);
 
 	}
 
